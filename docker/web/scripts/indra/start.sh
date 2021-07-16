15205b261012aaf609c0dcbe11698a732cb9cd9b
Hossein Bagherzadegan <h.bagherzadegan@ebuero.ag>
2021-07-17 01:41:29 +0200

    -init

--- docker/web/scripts/indra/start.sh
+++ docker/web/scripts/indra/start.sh
@@ -5,26 +5,20 @@ echo '--------------------- config usermod for apache --------------------------
 echo '-----------------------------------------------------------------------------------'
 # shellcheck disable=SC1035
 if [ ! -z "$WWWUSER" ]; then
-  usermod -u $WWWUSER sail
+  usermod -u $WWWUSER indra
+fi
+if [ $# -gt 0 ]; then
+  exec gosu $WWWUSER "$@"
 fi
 if [ ! -d /.composer ]; then
   mkdir /.composer
 fi
-
+echo '-----------------------------------------------------------------------------------'
+echo '--------------------- set composer permissions -----------------------------------'
+echo '-----------------------------------------------------------------------------------'
 chmod -R ugo+rw /.composer
-
-if [ $# -gt 0 ]; then
-  exec gosu $WWWUSER "$@"
-else
-  /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-fi
+echo 'composer folder permissions set successfully'
 echo '-----------------------------------------------------------------------------------'
-echo '----------------------- config apache modules -------------------------------------'
+echo '------------------------- start apache server -------------------------------------'
 echo '-----------------------------------------------------------------------------------'
-a2enmod rewrite
-a2enmod headers
-a2enmod expires
-a2enmod ssl
-a2enmod proxy_fcgi setenvif
-a2enmod php8.0
 /usr/sbin/apache2 -D FOREGROUND
